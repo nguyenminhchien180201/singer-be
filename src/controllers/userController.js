d1b1dbf50b96a6c650f02217e2efd05686a1449f
Minh Chien <nguyenminhchien180201@gmail.com>
2022-08-29 03:34:14 +0700

    two commit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -12,7 +12,6 @@ let handleLogin = async (req, res) => {
         })
     }
     let userData = await userService.handleUserLogin(email, password);
-    console.log('userDataadasf', userData);
     return res.status(200).json({
         errCode: userData.errCode,
         message: userData.errMessage,
